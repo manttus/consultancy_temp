240042f562f7fe0d38c837e89e614e12a2c657ba
manttus <rbasnyat666@gmail.com>
2023-06-09 13:46:11 +0545

    add swagger docs

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ import mongoose from "mongoose";
 import OtpRouter from "./routes/otp.js";
 import AuthRouter from "./routes/auth.js";
 import UserRouter from "./routes/user.js";
+import BlogRouter from "./routes/blog.js";
+import YAML from "yamljs";
+import swaggerUi from "swagger-ui-express";
 
 dotenv.config();
 
@@ -12,11 +15,15 @@ const port = process.env.PORT;
 const mongo_uri = process.env.MONGO_URI;
 const app = express();
 
+const swaggerDocument = YAML.load("./documentation.yaml");
+
 app.use(express.json());
 app.use(cors());
 app.use("/api/otp", OtpRouter);
 app.use("/api/auth", AuthRouter);
 app.use("/api/user", UserRouter);
+app.use("/api/blog", BlogRouter);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 mongoose.connect(mongo_uri).then(() => {
   console.log({ message: "Connected to MongoDB" });
