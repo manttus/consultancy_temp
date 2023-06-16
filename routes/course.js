0e0d85c66e518987b33ac99c11e8777ef8384e6c
manttus <rbasnyat666@gmail.com>
2023-06-16 15:34:16 +0545

    refactor: add search course route

--- routes/course.js
+++ routes/course.js
@@ -3,11 +3,13 @@ import express from "express";
 import {
   getCourse,
   addCourse,
+  searchCourses,
 } from "../controllers/courseController/courseController.js";
 
 const router = express.Router();
 
 router.get("/getCourse", getCourse);
 router.post("/addCourse", addCourse);
+router.get("/searchCourses/:Search", searchCourses);
 
 export default router;
