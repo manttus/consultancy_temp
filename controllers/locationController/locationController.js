a053384b3559c2a0ad4ef75b1b0f4c836329efe8
manttus <rbasnyat666@gmail.com>
2023-06-16 15:33:16 +0545

    refactor: change location controller to filters controller

--- controllers/locationController/locationController.js
+++ controllers/locationController/locationController.js
@@ -1,8 +1,10 @@
 import Location from "../../model/location.js";
-export const getLocation = async (req, res) => {
+import Degree from "../../model/degree.js";
+export const getFilterData = async (req, res) => {
   try {
-    const course = await Location.find();
-    return res.status(200).send({ locations: course });
+    const locations = await Location.find();
+    const degrees = await Degree.find();
+    return res.status(200).send({ locations, degrees });
   } catch (err) {
     return res.status(500).send({ message: "Server Error" });
   }
@@ -16,6 +18,19 @@ export const addLocation = async (req, res) => {
       location,
     }).save();
     return res.status(200).send({ message: "Location Added" });
+  } catch (err) {
+    return res.status(500).send({ message: "Server Error" });
+  }
+};
+
+export const addDegree = async (req, res) => {
+  const { degree } = req.body;
+  if (!degree) return res.status(400).send({ message: "Bad Request" });
+  try {
+    new Degree({
+      degree,
+    }).save();
+    return res.status(200).send({ message: "Degree Added" });
   } catch (err) {
     return res.status(500).send({ message: "Server Error" });
   }
