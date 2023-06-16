365e4cd4f454e6dc6a71548fad97f05cc4eefea2
manttus <rbasnyat666@gmail.com>
2023-06-16 16:17:27 +0545

    refactor: update missing routes

--- routes/course.js
+++ routes/course.js
@@ -4,6 +4,7 @@ import {
   getCourse,
   addCourse,
   searchCourses,
+  filterCourse,
 } from "../controllers/courseController/courseController.js";
 
 const router = express.Router();
@@ -11,5 +12,6 @@ const router = express.Router();
 router.get("/getCourse", getCourse);
 router.post("/addCourse", addCourse);
 router.get("/searchCourses/:Search", searchCourses);
+router.post("/filterCourses", filterCourse);
 
 export default router;
--- routes/location.js
+++ routes/location.js
@@ -3,11 +3,13 @@ import {
   getFilterData,
   addLocation,
   addDegree,
+  getCountries,
 } from "../controllers/locationController/locationController.js";
 
 const router = express.Router();
 router.get("/getFilters", getFilterData);
 router.post("/addLocation", addLocation);
 router.post("/addDegree", addDegree);
+router.get("/getCountries", getCountries);
 
 export default router;
